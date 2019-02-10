b8e2e71a683b7d11aa937ce9a85b2be1fba31128
Per Nilsson <per@pernilsson.io>
2019-02-09 18:14:16 -0800

    Replace website

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-40067241-3');
-
-exports.onRouteUpdate = (state, page, pages) => {
-    const { pathname } = state;
-    ReactGA.set({ page: pathname });
-    ReactGA.pageview(pathname);
-};
